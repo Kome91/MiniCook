bd0d8028b8ce92bf253d90357f36453b08771faa
y2210743 <y2210743@gl.cc.uec.ac.jp>
2025-02-05 22:11:09 +0900

    最新

--- Controller.java
+++ Controller.java
@@ -110,9 +110,9 @@ class DrawController implements KeyListener {
                 if (model.getGameTime() > 0) {
                     model.decreaseTime();
                     view.updateTime(model.getGameTime());
-                    if(model.getGameTime() <= 10){
+                    if(model.getGameTime() == 10){
                         AudioManager se = new AudioManager();
-                        se.playSE("./sound/music_timer.wav");
+                        se.playBGM("./sound/music_timer.wav");
                     }
                 } 
                 else {
